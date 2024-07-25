3ead8eaad6d4ee1e7f25b9f535bc5fc8ab40c9e6
JVSMOTA <jvmota.sb@gmail.com>
2024-07-25 15:00:05 -0300

    1005 - complete

---
+++ 1005/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**                                                                                                                                   * IMPORTANT:                                                                                                                         *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */                                                                                                                                  public class Main {                                                                                                                                                                                                                                                           public static void main(String[] args) throws IOException {
+                                                                                                                                             /**                                                                                                                                   * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí                                                                                                           */
+	Scanner scanner = new Scanner(System.in);
+
+        double pesoA = 3.5;
+        double pesoB = 7.5;
+	
+	double a = scanner.nextDouble();                                                                                                     double b = scanner.nextDouble();
+
+        System.out.printf("MEDIA = %.5f\n", ((a * pesoA + b * pesoB) / (pesoA + pesoB)));
+    }
+
+} 
+
