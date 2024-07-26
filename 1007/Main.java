45fec311eccbe872a66b66f2b92ff917cd9f1d51
JVSMOTA <jvmota.sb@gmail.com>
2024-07-26 17:14:35 -0300

    1007 - complete

---
+++ 1007/Main.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner; 
+
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+	Scanner scanner = new Scanner(System.in);
+    	
+	int a = scanner.nextInt();
+	int b = scanner.nextInt();
+	int c = scanner.nextInt();
+	int d = scanner.nextInt();
+
+	System.out.println("DIFERENCA = " + (a * b - c * d));
+    }
+ 
+}
