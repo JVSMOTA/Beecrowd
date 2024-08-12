64d4b4ed2d93031eae8e5def38d0b3111c7c3c2f
JVSMOTA <jvmota.sb@gmail.com>
2024-08-12 20:56:49 -0300

    :sparkles: new: updating run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,34 @@
 
 # Verifica se o argumento foi passado
 if [ -z "$1" ]; then
-  echo "Uso: bash build.sh <diretório>"
+  echo "Uso: bash run.sh <diretório ou caminho para Main.java>"
   exit 1
 fi
 
-# Acessa o diretório passado como argumento
-DIR=$1
+# Acessa o argumento passado
+ARG=$1
+
+# Verifica se é um diretório ou um arquivo
+if [ -d "$ARG" ]; then
+  DIR=$ARG
+  FILE="$DIR/Main.java"
+elif [ -f "$ARG" ]; then
+  FILE=$ARG
+  DIR=$(dirname "$ARG")
+else
+  echo "Erro: Argumento inválido. Passe um diretório ou o caminho para Main.java."
+  exit 1
+fi
 
 # Compila o arquivo Main.java
-javac $DIR/Main.java
+javac "$FILE"
 
-# Executa a classe Main
-java -cp $DIR Main
+# Verifica se a compilação foi bem-sucedida
+if [ $? -eq 0 ]; then
+  # Executa a classe Main
+  java -cp "$DIR" Main
+else
+  echo "Erro na compilação do arquivo."
+  exit 1
+fi
 
