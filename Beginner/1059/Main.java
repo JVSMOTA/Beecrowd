9ff79859ae77545debe202f6f930567cacb97a17
JVSMOTA <jvmota.sb@gmail.com>
2024-08-14 21:30:54 -0300

    :sparkles: feat: 1059 - complete

---
+++ Beginner/1059/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		int count = 2;
+		while (count <= 100) {
+			System.out.println(count);
+			count++;
+		}
+		
+	}
+}
+
