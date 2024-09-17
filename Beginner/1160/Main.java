b05a35a82122748e7b6e8d2f369eefda4a81af72
JVSMOTA <jvmota.sb@gmail.com>
2024-09-17 10:37:32 -0300

    ✨ feat: 1160 - Complete

--- Beginner/1160/Main.java
+++ Beginner/1160/Main.java
@@ -6,6 +6,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 		
-    }
+		int casos = scanner.nextInt();
 
+		for (int i = 0; i < casos; i++) {
+			int pA = scanner.nextInt();
+			int pB = scanner.nextInt();
+			double g1 = scanner.nextDouble();
+			double g2 = scanner.nextDouble();
+			
+			int years = 0;
+			while (true) {
+				if (years > 100) {
+					System.out.println("Mais de 1 seculo.");
+					break;
+				} else {
+					if (pA > pB) {
+						System.out.printf("%d anos.\n", years);
+						break;
+					}
+				}
+				
+				pA += Math.floor(pA * g1 / 100);
+				pB += Math.floor(pB * g2 / 100);
+
+				years++;
+			}
+		}
+	}
 }
