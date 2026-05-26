f4ce8a8187369c65c41a05b952ff721da33b6987
JVSMOTA <jvmota.sb@gmail.com>
2026-05-26 08:32:26 -0300

    swift introductions

---
+++ Beginner/1001/main.swift
@@ -0,0 +1,7 @@
+if let inputA = readLine(), let inputB = readLine(),
+   let a = Int(inputA), let b = Int(inputB) {
+    let soma = a + b
+    print("X = \(soma)")
+} else {
+    print("Entrada inválida. Digite números inteiros.")
+}
