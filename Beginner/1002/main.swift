1829205dc8ba8559839ff6cd91d15fe6fcd05583
JVSMOTA <jvmota.sb@gmail.com>
2026-05-26 08:53:21 -0300

    swift introductions

--- Beginner/1002/main.swift
+++ Beginner/1002/main.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 if let inputRaio = readLine(), let raio = Double(inputRaio) {
-    let area = Double.pi * raio * raio
+    let area = 3.14159 * raio * raio
     print(String(format: "A=%.4f", area))
 } else {
     print("Entrada incorreta! Digite um número válido (use ponto como separador decimal).")
